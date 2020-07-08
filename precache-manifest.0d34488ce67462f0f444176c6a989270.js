bfdf706c8b39bb31a5a766871029c127324780d5
febriliankr <febrilian.kr@gmail.com>
2020-07-08 12:11:27 +0700

    Updates

---
+++ precache-manifest.0d34488ce67462f0f444176c6a989270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c436d3190eb6e738702f9d32e0a460d3",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "68d60063bff884d3c260",
+    "url": "/react-deploy/static/css/main.dfa5b030.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/react-deploy/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d60063bff884d3c260",
+    "url": "/react-deploy/static/js/main.69fd91d8.chunk.js"
+  },
+  {
+    "revision": "86b36fb403cf01b938c6",
+    "url": "/react-deploy/static/js/runtime-main.c2a78e9b.js"
+  }
+]);
