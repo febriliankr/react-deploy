d7f90bb432d2f4ccc029416d6ba0cfc4b417aff4
febriliankr <febrilian.kr@gmail.com>
2020-07-08 10:58:38 +0700

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField : ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount (){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = (e) =>{
+    this.setState({searchField: e.target.value });
+  }
+
+  render(){
+    const { monsters, searchField } = this.state;
+    const filtered = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+      return(
+        <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+        placeholder = 'search monsters'
+        handleChange = {this.handleChange}
+        />
+        <CardList monsters={filtered}></CardList>
+        <h2>The History of Monsters Rolodex</h2>
+        <p>So my name is Febrilian Kristiawan, and I'm currently learning react. It's been hard because everything is pretty new for me, but I know I can go through the course here. Wish me luck!</p>
+        </div>
+      )
+  }
 }
 
 export default App;
